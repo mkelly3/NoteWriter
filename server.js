838b7261eafd24a34c2ddd787019a225260c5475
Morgan Kelly <mkelly5380@gmail.com>
2022-10-26 10:47:30 -0500

    installed path in npm and changed the directory so on start button it takes youto the notes page

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT =  process.env.PORT || 3001;
 
 var notesInfo; 
 
@@ -15,23 +16,39 @@ app.use(express.static('public'));
 
 //using express to get dat from index.html 
 app.get("/", (req, res) => {
-    res.sendFile(path.join("./public/index.html"))
+    res.sendFile(path.join(__dirname,"./public/index.html"))
 });
 
 //using express to get dat from notes.html 
-app.get("/", (req, res) => {
-    res.sendFile(path.join("./public/notes.html"))
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname,"./public/notes.html"))
 });
 
 
 //Parsing through notes data and posting it on the notes.html 
 
 app.get("/api/notes",(req, res) => {
-    fs.readFileSync("./db/db.json","utf-8").then(function(response){
+    fs.readFileSync(path.join(__dirname,"./db/db.json","utf-8")).then(function(response){
         notesInfo = JSON.parse(response);
         res.json(notesInfo);
     });
+});
+// Post to the notes page
+app.post("/api/notes",(req, res) => {
+    fs.readFileSync(path.join(__dirname,"./db/db.json","utf-8")).then(function(response){
+        //accessing the current note form the webpage 
+        const currentNote = req.body;
+        var currentId = notesInfo.length;
+        currentNote.id = currentId + 1;
 
+        notesInfo.push(currentNote);
+        notesInfo = JSON.stringify(notesInfo);
+        fs.writeFileSync("./db/db.json",notesInfo).then(function(dtat){
+            console.log("New Note Added!")
+        })
+
+        res.json(notesInfo);
+    });
 });
 
 // using express lisener to go to PORT 3001
